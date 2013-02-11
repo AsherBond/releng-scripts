292fc3e843b5fdddc462861dd4fbe6e5f79f7c63
Matt Spaulding <mspaulding06@gmail.com>
2013-02-11 13:44:23 -0800

    Added jenkins build scripts
    Restructured the jenkins directory so that it contains both images
    and scripts. The "scripts" directory will contain scripts that are
    to be executed by Jenkins build jobs.

---
+++ jenkins/scripts/build-eucalyptus.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+# Variables defined by Jenkins
+${platform:=centos-6-x86_64}
+${MODE:=prerelease}
+${GIT_BRANCH:=testing}
+${BUILD_TAG:=0}
+${BUILD_NUMBER:=0}
+
+git clone git://github.com/eucalyptus/eucalyptus.git
+
+# Git Repositories
+RPMFAB_REPO=git://github.com/gholms/rpmfab.git
+SPEC_REPO=git://github.com/eucalyptus/eucalyptus-rpmspec.git
+EUCAGIT="git --git-dir=eucalyptus/.git"
+
+if [ ! -d eucalyptus-cloud-libs ]; then
+    git clone git://github.com/eucalyptus/eucalyptus-cloud-libs.git
+fi
+
+git --git-dir=eucalyptus-cloud-libs/.git archive \
+    --format=tar.gz HEAD > cloud-lib.tar.gz
+
+# Tarball
+rm -f git-info.properties
+rm -rf rpmfab
+
+if [ -n "$commit_override" ]; then
+    $EUCAGIT clean -f
+    $EUCAGIT checkout -f $commit_override
+fi
+
+cat > git-info.properties << EOF
+MODE=$MODE
+GIT_URL=$($EUCAGIT remote show -n origin | sed -n '/Fetch URL:/s/^[^:]*: //p')
+GIT_BRANCH=$GIT_BRANCH
+GIT_COMMIT=$($EUCAGIT rev-parse HEAD)
+GIT_ABBREV_COMMIT=$($GIT_COMMIT | cut -c-8)
+GIT_COMMIT_LENGTH=$($EUCAGIT log --oneline --first-parent | wc -l)
+TARBALL_VERSION=$(cat eucalyptus/VERSION)
+TARBALL_SUFFIX=$(eval echo $tar_suffix)
+EOF
+
+eval `cat git-info.properties`
+
+$EUCAGIT archive --format=tar.gz \
+    --prefix=eucalyptus-${TARBALL_VERSION}${TARBALL_SUFFIX}/ \
+    HEAD > eucalyptus-${TARBALL_VERSION}${TARBALL_SUFFIX}.tar.gz
+
+# Eucalyptus SRPM
+git clone $RPMFAB_REPO rpmfab
+
+BUILD_ID=
+TARBALL_OPT=
+
+if [ "$TARBALL_SUFFIX" != "" ]; then
+    COMMON_OPTS="$COMMON_OPTS -m \"tar_suffix=${TARBALL_SUFFIX}\""
+fi
+
+if [ "$MODE" = "prerelease" ]; then
+    BUILD_ID="0.${BUILD_NUMBER}.@DATE@git${GIT_ABBREV_COMMIT}"
+elif [ "$MODE" = "postrelease" ]; then
+    BUILD_ID="${BUILD_NUMBER}.@DATE@git${GIT_ABBREV_COMMIT}"
+elif [ "$MODE" = "release" ]; then
+    if [ -n "$TARBALL_SUFFIX" ]; then
+        BUILD_ID="${BUILD_NUMBER}.$(echo $TARBALL_SUFFIX | sed 's/^-*//')"
+    else
+        BUILD_ID="${BUILD_NUMBER}"
+    fi
+fi
+
+platform_url=http://jenkins.release.eucalyptus-systems.com/userContent/mock/$platform.cfg
+
+rpmfab/build-srpm-from-scm.py -c "$platform_url" -w . -o results \
+    --mock-options "--uniqueext $BUILD_TAG" -m "build_id=$BUILD_ID" \
+    -m "abi_version=${GIT_COMMIT}" \
+    $TARBALL_OPT \
+    -f eucalyptus-*.tar.gz -f cloud-lib.tar.gz \
+    "${SPEC_REPO}#${GIT_BRANCH}"
+
+# Eucalyptus RPM
+rpmfab/build-arch.py -c "$platform_url" -o results \
+    --mock-options "--uniqueext $BUILD_TAG" results/*.src.rpm
+
